0e180cb023efa33aa9c873f53f441842adf5009f
haris.tiric <haris.harisaga@gmail.com>
2024-03-09 19:29:46 +0100

    w

---
+++ f-sharp-interactive/helloworld.fsx
@@ -0,0 +1,5 @@
+printfn "Hello World"
+
+printfn "Hello F#"
+
+// you can execute one line by pressing alt enter on it, or execute many by selecting and doing the same
---
+++ object-programming/interfaces.fsx
@@ -0,0 +1,66 @@
+(* Interfaces *)
+#r "nuget:FSharp.Data"
+
+open FSharp.Data
+open System.IO
+
+type IHtmlParser = 
+    abstract member ParseHtml : string -> HtmlDocument
+
+type WebParser () =
+    interface IHtmlParser with
+        member this.ParseHtml url = HtmlDocument.Load(url)
+
+    member this.ParseHtml url = (this :> IHtmlParser).ParseHtml(url)
+
+type FileParser () =
+    interface IHtmlParser with
+        member this.ParseHtml filePath =
+            filePath
+            |> File.ReadAllText
+            |> fun fileContents-> HtmlDocument.Parse(fileContents)
+
+    member this.ParseHtml filePath = (this :> IHtmlParser).ParseHtml(filePath)
+
+// Create Parsers
+let classWebParser = WebParser() :> IHtmlParser
+let classFileParser = FileParser() :> IHtmlParser
+
+// function to handle parsing of HTML
+let parseHtml (parser:IHtmlParser) (source:string) = parser.ParseHtml(source)
+
+// Use web parser
+parseHtml classWebParser "https://github.com/dotnet/fsharp"
+
+// use file parser
+Path.Join(__SOURCE_DIRECTORY__, "fsharp-github-repo.html")
+|> parseHtml classFileParser
+
+(* Object Expressions *)
+
+// Implements IHtmlParser to parse HTML contents from URL
+let webParser =
+    { new IHtmlParser with
+        member this.ParseHtml url = HtmlDocument.Load(url) }
+
+
+// implements IHtmlParser to Parse HTML contents from local file
+let fileParser =
+    { new IHtmlParser with
+        member this.ParseHtml filepath = 
+            filepath
+            |> File.ReadAllText
+            |> fun fileContents -> HtmlDocument.Parse(fileContents) }
+
+// parse wirh webParser
+parseHtml webParser "https://github.com/dotnet/fshaprp"
+
+// read local file contents and parse with fileParser
+Path.Join(__SOURCE_DIRECTORY__, "fshapr-github-repo.html")
+    |> parseHtml fileParser
+
+
+
+
+
+
